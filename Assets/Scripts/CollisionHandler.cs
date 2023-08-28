6559f455a106f3d07ddf731693a9def7c869e49a
Muzrottle <muzrottle@gmail.com>
2023-08-28 22:59:21 +0300

    Added Scenes and Managed Them
    Added Scenes and Managed Them

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -14,7 +14,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("You collected a Fuel.");
                 break;
             case "Finish":
-                Debug.Log("You finished the game.");
+                LoadNextLevel();
                 break;
             default:
                 ReloadLevel();
@@ -28,5 +28,20 @@ public class CollisionHandler : MonoBehaviour
 
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        Debug.Log("You finished the game.");
+        
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
